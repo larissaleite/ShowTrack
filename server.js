21cbbdbfe134f5eb231b395db259a1475f22967d
larissaleite <larissa.leite@gmail.com>
2014-07-29 14:36:16 -0300

    AngularJS and Express routing

--- server.js
+++ server.js
@@ -30,6 +30,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 
+app.get('*', function(req, res) {
+  // Use res.sendfile, as it streams instead of reading the file into memory.
+  res.sendfile('./public/index.html');
+});
+
 app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
