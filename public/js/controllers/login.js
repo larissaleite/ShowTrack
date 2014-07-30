76499b7ec9684f9c9ef24b4613999686e66ec7af
larissaleite <larissa.leite@gmail.com>
2014-07-30 00:30:06 -0300

    Basic signup and login

--- public/js/controllers/login.js
+++ public/js/controllers/login.js
@@ -1,4 +1,26 @@
 angular.module('showTrack')
-  .controller('LoginCtrl', ['$scope', function($scope) {
+  .controller('LoginCtrl', ['$scope', '$http', function($scope, $http) {
+
+  	$scope.login = function() {
+		var username = $scope.username;
+		var password = $scope.password;
+
+		var data = {
+                'username': username,
+                'password': password
+            };
+
+            console.log("Data " + JSON.stringify(data));
+
+            $http.post('/api/login', data)
+            .success(function(data) {
+                console.log("angularjs success");
+                console.log(data);
+                if (!data.length)
+                	window.location = '/login';
+                else
+                	window.location = '/home';
+            });
+	}
 
 }]);
